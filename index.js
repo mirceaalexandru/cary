60ad3e96d0c573dd3cf3f0454839885c7cfb90e3
malex <mircea.alexandru@gmail.com>
2016-10-20 11:20:09 +0300

    load sepratly db plugin

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const relativePath = Path.join(__dirname, 'public/');
 const Hapi = require('hapi');
 const ClientRoutes = require('./server/api/index');
 
-const Plugins = require('./plugins');
-
 var Server = new Hapi.Server({
 	app: Config
 });
@@ -18,51 +16,58 @@ var port = Config.web.port;
 Server.connection({port: port});
 Server.log(['error', 'database', 'read']);
 
-Server.register(Plugins.plugins, (err) => {
-	endIfErr(err);
+Server.register(
+	[
+		{
+			register: require('./plugins')
+		},
+		{
+			register: require('server-frame-mongo')
+		}
+	], (err) => {
+		endIfErr(err);
 
-//	Server.bind({config: Config});
-	Server.realm.settings.files.relativeTo = relativePath;
+		Server.realm.settings.files.relativeTo = relativePath;
 
-	const cache = Server.cache({segment: 'sessions', expiresIn: 3 * 24 * 60 * 60 * 1000});
-	Server.app.cache = cache;
+		const cache = Server.cache({segment: 'sessions', expiresIn: 3 * 24 * 60 * 60 * 1000});
+		Server.app.cache = cache;
 
-	Server.auth.strategy('session', 'cookie', true, {
-		password: 'password-should-be-32-characters',
-		isSecure: false,
-		validateFunc: function (request, session, callback) {
-			cache.get(session.sid, (err, cached) => {
-				if (err) {
-					return callback(err, false);
-				}
-
-				if (cached) {
-					return callback(null, true, cached.account);
-				}
-
-				var Session = Server.plugins.session.instance;
-				Session.get(session.sid, (err, data) => {
+		Server.auth.strategy('session', 'cookie', true, {
+			password: 'password-should-be-32-characters',
+			isSecure: false,
+			validateFunc: function (request, session, callback) {
+				cache.get(session.sid, (err, cached) => {
 					if (err) {
 						return callback(err, false);
 					}
 
-					if (data) {
-						cache.set(session.sid, { account: data })
-						return callback(null, true, data);
+					if (cached) {
+						return callback(null, true, cached.account);
 					}
 
-					return callback(null, false);
-				})
-			});
-		}
+					var Session = Server.plugins.session.instance;
+					Session.get(session.sid, (err, data) => {
+						if (err) {
+							return callback(err, false);
+						}
+
+						if (data) {
+							cache.set(session.sid, {account: data})
+							return callback(null, true, data);
+						}
+
+						return callback(null, false);
+					})
+				});
+			}
+		});
+
+		Server.route(ClientRoutes);
+
+		Server.app.logger.info(`Using config: ${JSON.stringify(Config, null, 2)}`);
+		Server.start(endIfErr);
+		Server.app.logger.info(`Server started at ${port}`);
 	});
-
-	Server.route(ClientRoutes);
-
-	Server.app.logger.info(`Using config: ${JSON.stringify(Config, null, 2)}`);
-	Server.start(endIfErr);
-	Server.app.logger.info(`Server started at ${port}`);
-});
 
 function endIfErr(err) {
 	if (err) {
--- plugins.js
+++ plugins.js
@@ -1,33 +1,34 @@
 'use strict'
 
-var plugins = [
-	{
-		register: require('hapi-auth-cookie')
-	},
-	{
-		register: require('bell')
-	},
-	{
-		register: require('inert')
-	},
-	{
-		register: require('hapi-pino')
-	},
-	{
-		register: require('server-frame-auth')
-	},
-	{
-		register: require('server-frame-mongo')
-	},
-	{
-		register: require('server-frame-utils')
-	},
-	{
-		register: require('lout'), options: {apiVersion: require('./package.json').version}
-	},
-	{
-		register: require('vision')
-	}
-];
+exports.register = function (server, options, next) {
+	server.register([
+		{
+			register: require('hapi-auth-cookie')
+		},
+		{
+			register: require('bell')
+		},
+		{
+			register: require('inert')
+		},
+		{
+			register: require('hapi-pino')
+		},
+		{
+			register: require('server-frame-auth')
+		},
+		{
+			register: require('server-frame-utils')
+		},
+		{
+			register: require('lout'), options: {apiVersion: require('./package.json').version}
+		},
+		{
+			register: require('vision')
+		}
+	], next);
+};
 
-exports.plugins = plugins;
+exports.register.attributes = {
+	name: 'default-plugins'
+};
